769725febd1dcaef29b115b7d9e0f426ab68a39d
dan-gheorghita <79468719+dan-gheorghita@users.noreply.github.com>
2026-03-15 13:22:54 +0200

    Add Python script

---
+++ vampire2.py
@@ -0,0 +1,8 @@
+if __name__ == 'Alice':  # Check if the username is Alice
+    print('Hi, Alice.')  # Print a personalized greeting for Alice
+elif age < 12:  # Check if the user is a child
+    print('You are not Alice, kiddo.')  # Print an age-based greeting
+elif age > 100:  # Check if the user is a senior
+    print('You are not Alice, grannie.')  # Print another age-based greeting
+elif age > 2000:  # Check for absurd age (to represent a vampire)
+    print('Unlike you, Alice is not an undead, immortal vampire.')  # Print a humorous message
